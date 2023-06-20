f728150f9a5670f53922886cd3e59bd4842efc0e
Walter Pagani <paganiwalter@gmail.com>
2023-06-20 00:51:51 -0300

    feat. Compatibility with the latest emulator version (#3)
    * feat. Compatibility with the latest emulator version
    
    * update README.md
    
    * fatal error: 'GetBoolDefault' is deprecated
    
    * fatal error: 'GetIntDefault' is deprecated

--- src/dynamicxp.cpp
+++ src/dynamicxp.cpp
@@ -4,6 +4,7 @@ Script reworked by Micrah/Milestorme and Poszer (Poszer is the Best)
 Module Created by Micrah/Milestorme
 Original Script from AshmaneCore https://github.com/conan513 Single Player Project 
 */
+
 #include "Configuration/Config.h"
 #include "ScriptMgr.h"
 #include "Player.h"
@@ -12,49 +13,39 @@ Original Script from AshmaneCore https://github.com/conan513 Single Player Proje
 
 class spp_dynamic_xp_rate : public PlayerScript
 {
-    public:
-        spp_dynamic_xp_rate() : PlayerScript("spp_dynamic_xp_rate") { };
+public:
+    spp_dynamic_xp_rate() : PlayerScript("spp_dynamic_xp_rate") { };
 
-        void OnLogin(Player* player) override
-       {
-            if (sConfigMgr->GetBoolDefault("Dynamic.XP.Rate.Announce", true))
-               {
-               ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00Level Dynamic XP |rmodule.");
-               }
-       }
-        void OnGiveXP(Player* player, uint32& amount, Unit* /*victim*/) override
-       {
-        if (sConfigMgr->GetBoolDefault("Dynamic.XP.Rate", true))
-        
-	   {
-           if (player->getLevel() <= 9)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.1-9", 1);
-
-           else if (player->getLevel() <= 19)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.10-19", 2);
-
-           else if (player->getLevel() <= 29)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.20-29", 3);
-
-           else if (player->getLevel() <= 39)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.30-39", 4);
-
-           else if (player->getLevel() <= 49)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.40-49", 5);
-
-           else if (player->getLevel() <= 59)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.50-59", 6);
-
-           else if (player->getLevel() <= 69)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.60-69", 7);
-
-           else if (player->getLevel() <= 79)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.70-79", 8);
-
+    void OnLogin(Player* player) override
+    {
+        if (sConfigMgr->GetOption<bool>("Dynamic.XP.Rate.Announce", true))
+        {
+            ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00Level Dynamic XP |rmodule.");
         }
+    }
 
-     }
-    
+    void OnGivePlayerXP(Player* player, uint32& amount, Unit* /*victim*/)
+    {
+        if (sConfigMgr->GetOption<bool>("Dynamic.XP.Rate", true))
+        {
+            if (player->getLevel() <= 9)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.1-9", 1);
+            else if (player->getLevel() <= 19)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.10-19", 2);
+            else if (player->getLevel() <= 29)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.20-29", 3);
+            else if (player->getLevel() <= 39)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.30-39", 4);
+            else if (player->getLevel() <= 49)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.40-49", 5);
+            else if (player->getLevel() <= 59)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.50-59", 6);
+            else if (player->getLevel() <= 69)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.60-69", 7);
+            else if (player->getLevel() <= 79)
+                amount *= sConfigMgr->GetOption<uint32>("Dynamic.XP.Rate.70-79", 8);
+        }
+    }
 };
 
 void AddSC_dynamic_xp_rate()
